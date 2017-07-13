09a8ff072255a76deb00a8e0c720dfda8a28bb6e
SlevinKelevra <poztalion2@yandex.ru>
2017-07-13 22:24:09 +0300

    Add show, new, create, destroy, edit to flashcard

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,6 +1,45 @@
 class FlashcardsController < ApplicationController
+  before_action :set_flashcard, only: [:show, :edit, :destroy]
+
   def index
     @flashcards = Flashcard.all
+  end
+
+  def new
+    @flashcard = Flashcard.new
+  end
+
+  def create
+    @flashcard = Flashcard.new(flashcard_params)
+
+    if @flashcard.save
+      redirect_to @flashcard
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  def destroy
+    @flashcard.destroy
+
+    redirect_to flashcard_path
+  end
+
+  private
+
+  def set_flashcard
+    @flashcard = Flashcard.find(params[:id])
+  end
+
+  def flashcard_params
+    params.require(:flashcard).permit(:original_text, :translated_text)
   end
 
 end
