90306bcc7d46dc1c284c4c6cf0ddc215e1e30247
SlevinKelevra <poztalion2@yandex.ru>
2017-07-15 15:51:30 +0300

    Add validate to flashcard

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -3,4 +3,11 @@ class Flashcard < ApplicationRecord
     self.review_date = Time.now.days_since(3)
   end
 
+  validate :translated_text_can_not_be_equal_original_text
+
+  def translated_text_can_not_be_equal_original_text
+    if self.original_text.upcase == self.translated_text.upcase
+      errors.add(:translated_text, "can not be equal original text")
+    end
+  end
 end
