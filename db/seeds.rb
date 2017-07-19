bc29e12e6a49afefdaecbe6b0944175699a88de0
SlevinKelevra <poztalion2@yandex.ru>
2017-07-19 17:28:06 +0300

    Add 1000 Flashcard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+
+page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
+all_values = []
+pars_page = page.css('.entry-content tr')
+pars_page.each do |i|
+  one_value = []
+  one_value[0] = i.css('td')[1].text
+  one_value[1] = i.css('td')[2].text
+  all_values << one_value
+end
+
+all_values.each do |i|
+  Flashcard.create(translated_text: i[0], translated_text: i[1])
+end
