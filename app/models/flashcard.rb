6e5a6dba1847bb662e13672706c855e750d46498
SlevinKelevra <poztalion2@yandex.ru>
2017-07-20 20:07:19 +0300

    Fix 3

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,14 +1,17 @@
 class Flashcard < ApplicationRecord
   before_validation do
-    self.review_date = Time.now.days_since(3)
+    self.review_date = Time.now.days_since(3) if self.review_date.nil?
   end
 
   validate :translated_text_can_not_be_equal_original_text
   validates :original_text, :translated_text, :review_date, presence: true
 
   def translated_text_can_not_be_equal_original_text
-    if self.original_text.upcase == self.translated_text.upcase
-      errors.add(:translated_text, 'can not be equal original text')
+    if self.original_text.nil? || self.translated_text.nil?
+    else
+      if self.original_text.upcase == self.translated_text.upcase
+        errors.add(:translated_text, 'can not be equal original text')
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,8 @@ require 'nokogiri'
 
 
 page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
-all_values = []
 pars_page = page.css('.entry-content tr')
 pars_page.each do |one_value|
-  all_values << [one_value.css('td')[1].text, one_value.css('td')[2].text]
+  Flashcard.create(original_text: one_value.css('td')[1].text, translated_text: one_value.css('td')[2].text)
 end
 
-all_values.each do |one_value|
-  Flashcard.create(original_text: one_value[0], translated_text: one_value[1])
-end
